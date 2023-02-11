335bd848f635a986f3a9f95eafa48109b07e1dc9
KJY <kid4211@gmail.com>
2023-02-11 14:24:33 +0900

    #16 - 게시판 뷰 기본 틀 작성
    - controller handler method를 통해 테스트를 통과하게 작성
    - 적절한 뷰 틀을 잡음
    - 테스트에서는 media type을 유연하게 검사하도록 수정
    - thymeleaf 컴포넌트화 기능 (th: replace)을 통해 반복이 예상되는 파일 분리

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -23,9 +23,10 @@ class ArticleControllerTest {
     }
 
     /**
-     * Disabled - 부분적으로 테스트 메서드를 비활성화 시킬수 있는 annotation
+     * @Disabled - 부분적으로 테스트 메서드를 비활성화 시킬수 있는 annotation
+     * 기존 테스트를 실패한 채 올리면 `gradle.build`가 실패하여 배포 자동화에 악영향을 끼치므로, 실패 테스트는 테스트 제외 처리한다.
      */
-    @Disabled
+
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -34,7 +35,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -48,7 +49,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -61,7 +62,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 }
